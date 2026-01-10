0bb5f048a10474c22f5bc5eb7e0bca0814cb8176
Michael Vincent <mhvincent.2604@gmail.com>
2026-01-09 20:24:40 -0600

    finally fixed tmux navigator

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -25,7 +25,3 @@ map("n", "<leader>m", "<cmd>bn<CR>", {desc = "Next buffer"})
 map("n", "<leader>u", "<cmd>bd<CR>", {desc = "Close buffer"})
 map("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Show diagnostics in float" })
 
-map("n", "<M-h>", "<cmd>wincmd h<CR>", {desc = "Focus on left window"})
-map("n", "<M-j>", "<cmd>wincmd j<CR>", {desc = "Focus on down window"})
-map("n", "<M-k>", "<cmd>wincmd k<CR>", {desc = "Focus on up window"})
-map("n", "<M-l>", "<cmd>wincmd l<CR>", {desc = "Focus on right window"})
--- config/nvim/lua/plugins/tmux-nvim-navigator.lua
+++ config/nvim/lua/plugins/tmux-nvim-navigator.lua
@@ -1,7 +1,11 @@
-return{
-    "christoomey/vim-tmux-navigator",
-    vim.keymap.set('n', '<M-h>', ':TmuxNavigateLeft<CR>'),
-    vim.keymap.set('n', '<M-j>', ':TmuxNavigateDown<CR>'),
-    vim.keymap.set('n', '<M-k>', ':TmuxNavigateUp<CR>'),
-    vim.keymap.set('n', '<M-l>', ':TmuxNavigateRight<CR>'),
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
+  config = function()
+    vim.g.tmux_navigator_no_mappings = 1
+    vim.keymap.set('n', '<M-h>', ':<C-U>TmuxNavigateLeft<CR>')
+    vim.keymap.set('n', '<M-j>', ':<C-U>TmuxNavigateDown<CR>')
+    vim.keymap.set('n', '<M-k>', ':<C-U>TmuxNavigateUp<CR>')
+    vim.keymap.set('n', '<M-l>', ':<C-U>TmuxNavigateRight<CR>')
+  end,
 }
