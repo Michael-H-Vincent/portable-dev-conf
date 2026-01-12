6c237f0dbf343cc8410e74e641f3f8da845fd16a
Michael Vincent <mhvincent.2604@gmail.com>
2026-01-12 15:14:58 -0600

    pushed some more custom keybinds

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -19,9 +19,13 @@ map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })    -- Format is ( mo
 map("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit window" })
 
 -- Insert mode: typing "jj" quickly goes back to normal mode
-map("i", "jj", "<Esc>", { desc = "Exit insert mode with jk" })
+map("i", "jj", "<Esc>", { desc = "Exit insert mode with jj" })
 map("n", "<leader>n", "<cmd>bp<CR>", {desc = "Previous buffer"})
 map("n", "<leader>m", "<cmd>bn<CR>", {desc = "Next buffer"})
 map("n", "<leader>u", "<cmd>bd<CR>", {desc = "Close buffer"})
 map("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Show diagnostics in float" })
 
+map("x", ">", ">gv", {desc = "shift block and then reselect"})
+map("x", "<", "<gv", {desc = "shift block and then reselect"})
+
+map("n", "<CR>", "o<Esc>", {desc = "actually useful usage of CR"})
